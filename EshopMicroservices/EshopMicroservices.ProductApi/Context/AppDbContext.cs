67eb174cb3c698571d1af1bc281972be62dca76e
Lucas-RM <lucas.marcondes36@gmail.com>
2025-08-15 21:02:43 -0300

    feat: implement Entity Framework Core configuration and initial migrations
    - Add Fluent API configuration in AppDbContext
    - Create initial migration for Products and Categories tables
    - Implement data seeding for categories and products
    - Configure entity relationships with cascade delete

--- EshopMicroservices/EshopMicroservices.ProductApi/Context/AppDbContext.cs
+++ EshopMicroservices/EshopMicroservices.ProductApi/Context/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using EshopMicroservices.ProductApi.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
 
 namespace EshopMicroservices.ProductApi.Context;
 
@@ -11,4 +12,55 @@ public class AppDbContext : DbContext
 
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
+
+    // Fluent API configuration
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Product configuration
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Description)
+            .HasMaxLength(255)
+            .IsRequired();
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.ImageURL)
+            .HasMaxLength(255)
+            .IsRequired();
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Price)
+            .HasPrecision(12, 2);
+
+        // Category configuration
+        modelBuilder.Entity<Category>()
+            .HasKey(c => c.CategoryId);
+
+        modelBuilder.Entity<Category>()
+            .Property(c => c.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        modelBuilder.Entity<Category>()
+            .HasMany(c => c.Products)
+            .WithOne(p => p.Category)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Category>().HasData(
+            new Category
+            {
+                CategoryId = 1,
+                Name = "Material Escolar"
+            },
+            new Category {
+                CategoryId = 2,
+                Name = "Acessórios"
+            }
+        );
+    }
 }
